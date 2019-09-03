ff9fd885712caa893e34648cc9ac484c162c2b3a
samsolarit <53542371+samsolarit@users.noreply.github.com>
2019-09-02 22:08:57 -0400

    Add files via upload

--- samuelpictures.py
+++ samuelpictures.py
@@ -1,10 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Welcome to the ATEN Box Informational Page. The Aten Box is a project by Samuel Scott, Jr. for Photovoltaic and Datascience purposes."
+
+@app.route('/index', methods=['GET', 'POST'])
+def pictures():
+    return app.send_static_file('webappindex.html')
 
 if __name__== "__main__":
         app.run(debug=True)
